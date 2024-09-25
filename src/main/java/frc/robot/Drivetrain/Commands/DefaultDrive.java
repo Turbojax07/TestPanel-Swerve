0961555cb37a90dc05ff59ae3e0da3ba44f55865
Turbojax07 <turbojax07@gmail.com>
2024-09-25 00:02:48 -0700

    Adding a bunch of math and logging the controller inputs

--- src/main/java/frc/robot/Drivetrain/Commands/DefaultDrive.java
+++ src/main/java/frc/robot/Drivetrain/Commands/DefaultDrive.java
@@ -1,48 +1,82 @@
 package frc.robot.Drivetrain.Commands;
 
-import java.util.function.BooleanSupplier;
-import java.util.function.DoubleSupplier;
-
+import java.util.function.Supplier;
+import frc.robot.Drivetrain.SwerveModule;
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Drivetrain.Drivetrain;
 
 public class DefaultDrive extends Command {
-
-    private final DoubleSupplier x_trans;
-    private final DoubleSupplier y_trans;
-    private final DoubleSupplier z_rot;
+    private final Supplier<Double> x_trans;
+    private final Supplier<Double> y_trans;
+    private final Supplier<Double> z_rotat;
 
     private final Drivetrain drivetrain;
 
-    public DefaultDrive(DoubleSupplier x_trans, DoubleSupplier y_trans, DoubleSupplier z_rot) {
+    public DefaultDrive(Supplier<Double> x_trans, Supplier<Double> y_trans, Supplier<Double> z_rot) {
         this.x_trans = x_trans;
         this.y_trans = y_trans;
-        this.z_rot = z_rot;
+        this.z_rotat = z_rot;
 
         this.drivetrain = Drivetrain.getInstance();
+        for (SwerveModule module : drivetrain.getModules()) {
+            module.setHeading(new Rotation2d());
+        }        
+
         addRequirements(drivetrain);
     }
 
     public void initialize() {}
 
     public void execute() {
-        drivetrain.drive(ChassisSpeeds.fromFieldRelativeSpeeds(
-            MathUtil.applyDeadband(x_trans.getAsDouble(), 0.2) * DriveConstants.maxDriveSpeed,
-            MathUtil.applyDeadband(y_trans.getAsDouble(), 0.2) * DriveConstants.maxDriveSpeed,
-            MathUtil.applyDeadband(z_rot.getAsDouble(), 0.2) * DriveConstants.maxTurnSpeed, 
-            drivetrain.getAngle()));
+        // Getting the inputs
+        double xSpeed = x_trans.get();
+        double ySpeed = y_trans.get();
+        double zRotat = z_rotat.get();
+
+        // Outputting the raw controller values to SmartDashboard
+        SmartDashboard.putNumber("/Controller/LeftX_Raw", xSpeed);
+        SmartDashboard.putNumber("/Controller/LeftY_Raw", ySpeed);
+        SmartDashboard.putNumber("/Controller/RightX_Raw", zRotat);
+
+        // Applying deadband and max speed to the xSpeed input
+        xSpeed = MathUtil.applyDeadband(-xSpeed, DriveConstants.deadband);
+        if (xSpeed > 0) xSpeed -= DriveConstants.deadband;
+        if (xSpeed < 0) xSpeed += DriveConstants.deadband;
+        xSpeed *= DriveConstants.maxDriveSpeed;
+
+        // Applying deadband and max speed to the ySpeed input
+        ySpeed = MathUtil.applyDeadband(-ySpeed, DriveConstants.deadband);
+        if (ySpeed > 0) ySpeed -= DriveConstants.deadband;
+        if (ySpeed < 0) ySpeed += DriveConstants.deadband;
+        ySpeed *= DriveConstants.maxDriveSpeed;
+
+        // Applying deadband and max speed to the zRotat input
+        zRotat = MathUtil.applyDeadband(-zRotat, DriveConstants.deadband);
+        if (zRotat > 0) zRotat -= DriveConstants.deadband;
+        if (zRotat < 0) zRotat += DriveConstants.deadband;
+        zRotat *= DriveConstants.maxTurnSpeed;
+
+        // Outputting the adjusted controller values to SmartDashboard
+        SmartDashboard.putNumber("/Controller/LeftX_Adjusted", xSpeed);
+        SmartDashboard.putNumber("/Controller/LeftY_Adjusted", ySpeed);
+        SmartDashboard.putNumber("/Controller/RightX_Adjusted", zRotat);
+
+        // Creating the ChassisSpeeds object.
+        ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, zRotat, drivetrain.getAngle());
+
+        // Driving the robot
+        drivetrain.drive(speeds);
     }
 
     public boolean isFinished() {
         return false;
     }
 
-    public void end(boolean interr) {
-        drivetrain.stop();
-    }
+    public void end(boolean interr) {}
     
 }
