90cce2046d23269badc0a5ba601868c800c7d5b4
turbojax07 <turbojax07@gmail.com>
2024-10-06 00:43:25 -0400

    Removing everything and implementing YAGSL

--- src/main/java/frc/robot/Drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain/Drivetrain.java
@@ -1,42 +1,18 @@
 package frc.robot.Drivetrain;
 
-import com.ctre.phoenix6.hardware.Pigeon2;
-import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
-import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
+import java.io.File;
+import java.io.IOException;
+
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.DriveConstants;
-import frc.robot.Constants.PhysicalConstants;
+import swervelib.SwerveDrive;
+import swervelib.parser.SwerveParser;
+import edu.wpi.first.wpilibj.Filesystem;
 
 public class Drivetrain extends SubsystemBase {
-    // Swerve Modules
-    private SwerveModule flModule;
-    private SwerveModule frModule;
-    private SwerveModule blModule;
-    private SwerveModule brModule;
-
-    // Arrays of data
-    private SwerveModule[] modules;
-    private SwerveModulePosition[] positions;
-
-    // Gyro
-    private Pigeon2 gyro;
-
-    // Kinematics
-    private SwerveDriveKinematics kinematics;
-
-    // Odometry / Pose Estimation
-    private SwerveDriveOdometry odometry;
-    private Pose2d initPose = new Pose2d();
-    private Field2d field;
+    // The YAGSL SwerveDrive controller.
+    private SwerveDrive swerveDrive;
 
     // A common instance of the drivetrain subsystem.
     private static Drivetrain instance;
@@ -56,54 +32,30 @@ public class Drivetrain extends SubsystemBase {
     }
 
     public Drivetrain() {
-        // Initializing the Swerve Modules
-        flModule = new SwerveModule("Front Left", DriveConstants.flDriveId, DriveConstants.flTurnId, DriveConstants.flEncoderId, DriveConstants.flEncoderOffset);
-        frModule = new SwerveModule("Front Right", DriveConstants.frDriveId, DriveConstants.frTurnId, DriveConstants.frEncoderId, DriveConstants.frEncoderOffset);
-        blModule = new SwerveModule("Back Left", DriveConstants.blDriveId, DriveConstants.blTurnId, DriveConstants.blEncoderId, DriveConstants.blEncoderOffset);
-        brModule = new SwerveModule("Back Right", DriveConstants.brDriveId, DriveConstants.brTurnId, DriveConstants.brEncoderId, DriveConstants.brEncoderOffset);
-
-        // Initializing the gyro
-        gyro = new Pigeon2(DriveConstants.gyroId);
-
-        // Creating the arrays
-        modules = new SwerveModule[] {flModule, frModule, blModule, brModule};
-        positions = new SwerveModulePosition[] {flModule.getSwerveModulePosition(), frModule.getSwerveModulePosition(), blModule.getSwerveModulePosition(), brModule.getSwerveModulePosition()};
-
-        // Initializing the kinematics
-        kinematics = new SwerveDriveKinematics(
-            new Translation2d( PhysicalConstants.robotWidth / 2,  PhysicalConstants.robotLength / 2),
-            new Translation2d( PhysicalConstants.robotWidth / 2, -PhysicalConstants.robotLength / 2),
-            new Translation2d(-PhysicalConstants.robotWidth / 2,  PhysicalConstants.robotLength / 2),
-            new Translation2d(-PhysicalConstants.robotWidth / 2, -PhysicalConstants.robotLength / 2)
-        );
-
-        // Initializing the odometry
-        odometry = new SwerveDriveOdometry(kinematics, getAngle(), positions, initPose);
-
-        // Initializing the 2d field
-        field = new Field2d();
-    }
-
-    @Override
-    public void periodic() {
-        odometry.update(getAngle(), positions);
+        // Initializing the drivetrain
+        try {
+            swerveDrive = new SwerveParser(new File(Filesystem.getDeployDirectory(), "swerve")).createSwerveDrive(DriveConstants.maxDriveSpeed);
+        } catch (IOException err) {
+            // If the config directory does not exist, the code fails and the program exits.
+            System.out.println("YAGSL config directory not found.  Please redeploy the code.");
+            System.exit(1);
+        }
 
 
-        field.setRobotPose(odometry.getPoseMeters());
+        // Enabling heading correction.
+        swerveDrive.setHeadingCorrection(true);
 
-        SmartDashboard.putData("/Field", field);
-        SmartDashboard.putNumber("Gyro", getAngle().getDegrees());
-    }
+        // Discretizing chassis speeds
+        swerveDrive.setChassisDiscretization(true, 0.2);
 
-    public Rotation2d getAngle() {
-        return Rotation2d.fromDegrees(gyro.getYaw().getValue());
+        // Disabling brake mode
+        swerveDrive.setMotorIdleMode(false);
+
+        // Setting the max turn speed
+        swerveDrive.swerveController.setMaximumAngularVelocity(DriveConstants.maxTurnSpeed);
     }
 
     public void drive(ChassisSpeeds speeds) {
-        SwerveModuleState[] states = kinematics.toSwerveModuleStates(speeds);
-
-        for (int i = 0; i < 4; i++) {
-            modules[i].setState(states[i]);
-        }
+        swerveDrive.driveFieldOriented(speeds);
     }
 }
