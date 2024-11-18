02544352f083b3b614b7b22c9eb38d33d73f0446
turbojax07 <turbojax07@gmail.com>
2024-11-17 22:41:13 -0500

    Using the new 2025 units!  Also adding a few more estimated constants to help with pathplanner stuff later.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,7 +4,16 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Dimensionless;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.units.measure.Mass;
+import edu.wpi.first.units.measure.MomentOfInertia;
+
+import static edu.wpi.first.units.Units.*;
 
 public final class Constants {
     public static final boolean replayEnabled = false;
@@ -13,51 +22,56 @@ public final class Constants {
         public static final int flDriveId = 1;
         public static final int flTurnId = 2;
         public static final int flEncoderId = 0;
-        public static final double flEncoderOffset = 0; // Rotations
+        public static final Angle flEncoderOffset = Rotations.of(0);
 
         public static final int frDriveId = 3;
         public static final int frTurnId = 4;
         public static final int frEncoderId = 1;
-        public static final double frEncoderOffset = 0; // Rotations
+        public static final Angle frEncoderOffset = Rotations.of(0);
 
         public static final int blDriveId = 5;
         public static final int blTurnId = 6;
         public static final int blEncoderId = 2;
-        public static final double blEncoderOffset = 0; // Rotations
+        public static final Angle blEncoderOffset = Rotations.of(0);
 
         public static final int brDriveId = 7;
         public static final int brTurnId = 8;
         public static final int brEncoderId = 3;
-        public static final double brEncoderOffset = 0.802; // Rotations
+        public static final Angle brEncoderOffset = Rotations.of(0);
 
         public static final int gyroId = 10;
+        
+        public static final LinearVelocity maxDriveSpeed = MetersPerSecond.of(1);
+        public static final AngularVelocity maxTurnSpeed = RadiansPerSecond.of(Math.PI);
 
-        public static final double maxDriveSpeed = 1; // Meters / Second
-        public static final double maxTurnSpeed = Math.PI; // Radians / Second
+        public static final Current driveCurrentLimit = Amps.of(35);
+        public static final Current steerCurrentLimit = Amps.of(35);
 
         public static final double driveP = 0.01;
         public static final double driveI = 0;
         public static final double driveD = 0;
         public static final double driveFF = 0.2;
 
-        public static final double turnP = 0.4;
-        public static final double turnI = 0;
-        public static final double turnD = 0;
-        public static final double turnFF = 0;
+        public static final double steerP = 0.4;
+        public static final double steerI = 0;
+        public static final double steerD = 0;
+        public static final double steerFF = 0;
 
-        public static final double deadband = 0.2;
+        public static final Dimensionless deadband = Percent.of(0.2);
     }
 
     public static class PhysicalConstants {
-        public static final double robotWidth = Units.inchesToMeters(10); // Meters
-        public static final double robotLength = Units.inchesToMeters(10); // Meters
-        public static final double wheelDiameter = Units.inchesToMeters(4); // Meters
+        public static final Mass mass = Pounds.of(50); // IDK the weight
+        public static final MomentOfInertia inertia = KilogramSquareMeters.of(50); // IDK the inertia
+        public static final Distance width = Meters.of(Inches.of(10).in(Meters));
+        public static final Distance length = Meters.of(Inches.of(10).in(Meters));
+        public static final Distance wheelRadius = Meters.of(Inches.of(2).in(Meters));
 
         public static final double driveGearRatio = 5.36;
         public static final double turnGearRatio = 150.0 / 7.0;
+    
+        public static final double driveRotToMeters = 2 * Math.PI * wheelRadius.in(Meters) / driveGearRatio;
 
-        public static final double driveRotToMeters = wheelDiameter * Math.PI / driveGearRatio; // met/rot
-
-        public static final double turnRotToRad = 2 * Math.PI / turnGearRatio; // rad/rot
+        public static final double steerRotToRad = 2 * Math.PI / turnGearRatio;
     }
 }
